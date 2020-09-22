5581ad03e231773027e7fd524660bdaed12c21ff
meriem farsi <farsi.meriem60@gmail.com>
2020-09-22 02:29:20 +0000

    update functions related to users

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,49 +1,59 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
 
-/// Users
-
-/**
- * Get a single user from the database given their email.
- * @param {String} email The email of the user.
- * @return {Promise<{}>} A promise to the user.
- */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+ /// Users
+  /**
+   * Get a single user from the database given their email.
+   * @param {String} email The email of the user.
+   * @return {Promise<{}>} A promise to the user.
+   */
+const getUserWithEmail = (email) => {
+    const query = {
+      text : `SELECT * FROM users WHERE email = $1`,
     }
-  }
-  return Promise.resolve(user);
-}
-exports.getUserWithEmail = getUserWithEmail;
+    return pool
+        .query(query, [email])
+        .then((result) => result.rows[0])
+        .catch((err) => err.message);
+  };
+  exports.getUserWithEmail = getUserWithEmail;
 
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
-}
+const getUserWithId = (id) => {
+  const query = {
+    text : `SELECT * FROM users WHERE id = $1`,
+  }
+  return pool
+      .query(query, [id])
+      .then((result) => result.rows)
+      .catch((err) => err.message);
+};
 exports.getUserWithId = getUserWithId;
-
-
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+const addUser =  (user) => {
+  const query = {
+    text : `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`
+  }
+  return pool
+      .query(query, [user.name, user.email, user.password])
+      .then((result) => result.rows)
+      .catch((err) => err.message);
+};
 exports.addUser = addUser;
 
 /// Reservations
