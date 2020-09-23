7f9a8e854fc7eeeee9f5172cadbcc74cc26aab1f
meriem farsi <farsi.meriem60@gmail.com>
2020-09-23 04:23:56 +0000

    LighthouseBnB add properties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -91,14 +91,6 @@ const getAllReservations = function(guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-//  const getAllProperties = function(options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// }
-
 const getAllProperties = function(options, limit = 10) {
   // 1
   const queryParams = [];
@@ -164,9 +156,17 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  
+  const query = `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url,
+    cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms,
+    number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`;
+  return pool
+      .query(query, [property.owner_id, property.title, property.description, property.thumbnail_photo_url,
+        property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province,
+        property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms,
+        property.number_of_bedrooms
+      ])
+      .then((result) => result.rows)
+      .catch((err) => err.message);
 }
 exports.addProperty = addProperty;
